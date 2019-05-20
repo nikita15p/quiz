1ea74d4d63247347fb52a7ebaa0fec123bac22b1
Nikita Pande <37657012+nikita15p@users.noreply.github.com>
2019-05-20 12:31:33 +0530

    Solution to problem1

--- main.go
+++ main.go
@@ -1,58 +1,77 @@
 package main
 
 import (
-	"encoding/csv"
+	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 )
 
+func readCsvFile(file string) (q []string, ans []string) {
+
+	r, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Errorf("csv file read error due to", err)
+	}
+
+	str := string(r)
+	arr := strings.Fields(str)
+	for _, v := range arr {
+		qa := strings.Split(v, ",")
+		q = append(q, qa[0])
+		ans = append(ans, qa[1])
+	}
+
+	return q, ans
+}
+
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
-	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
-	}
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse the provided CSV file.")
-	}
-	problems := parseLines(lines)
+	f := flag.Lookup("file").Value.String()
+	q, expected := readCsvFile(f)
+
+	var observed string
 
 	correct := 0
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+	total := len(q)
+	incorrect := total
+	fmt.Printf("Press Enter to start the test")
+	reader := bufio.NewReader(os.Stdin)
+	key, _, _ := reader.ReadRune()
+	if key == rune(10) {
+		timer := time.NewTimer(5 * time.Second)
+		go func(){
+			//If Enter Key is pressed, start the timer
+			for i, ques := range q{
+				fmt.Println("What is", ques, "=?")
+				fmt.Scanf("%s", &observed)
+				if observed == expected[i] {
+					correct++
+					incorrect--
+				}
+
+			}
+		}(	)
+
+
+			<-timer.C
 		}
+
+
+
+
+	fmt.Printf("Total Questions: %d Correct: %d Incorrect: %d", total, correct, incorrect)
+
+}
+
+func init() {
+	f := flag.Lookup("file")
+	if f == nil {
+		flag.String("file", "problems.csv", "Set the csv file name if not default")
 	}
-
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-}
-
-func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
-		}
-	}
-	return ret
-}
-
-type problem struct {
-	q string
-	a string
-}
-
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
 }
